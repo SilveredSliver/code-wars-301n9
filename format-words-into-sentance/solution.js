cb97f61bc4dbb70e0f690ae2ff9017fda1dde583
AaronBruce <aaron.bruce@comcast.net>
2017-12-28 17:44:38 -0800

    added and completed format-words-into-sentence kata

---
+++ format-words-into-sentance/solution.js
@@ -0,0 +1,33 @@
+function formatWords(words){
+    if (!words) {
+      return "";
+    };
+    words = words.filter(item => item.length !== 0);
+    console.log(words.length)
+    let stringOutput = '';
+    switch(words.length) {
+      case 0:
+        stringOutput = '';
+        break;
+      case 1:
+        stringOutput = words[0];
+        break;
+      case 2:
+        stringOutput = words[0].concat(' and ', words[1]);
+        break;
+      default:
+        let lastWord = words[words.length - 1];
+        stringOutput = words.slice(0, -1).join(', ');
+        stringOutput = stringOutput.concat(' and ', lastWord);
+        break;
+    };
+    console.log(stringOutput);
+    return (stringOutput);
+  };
+  //solution code
+
+
+  formatWords(['ninja', 'samurai', 'ronin']) // should return "ninja, samurai and ronin"
+  formatWords(['ninja', '', 'ronin']) // should return "ninja and ronin"
+  formatWords([]) // should return ""
+  //sample tests
