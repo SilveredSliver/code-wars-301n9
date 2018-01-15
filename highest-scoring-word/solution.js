78c9548027bffab4fe91ab97a5a07b4297787cfc
AaronBruce <aaron.bruce@comcast.net>
2018-01-14 20:22:11 -0800

    added and completed highest-scoring-word

---
+++ highest-scoring-word/solution.js
@@ -0,0 +1,35 @@
+function genCharArray(charA, charZ) {
+    var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
+    for (; i <= j; ++i) {
+        a.push(String.fromCharCode(i));
+    }
+    return a;
+}
+let alphabet = genCharArray('a', 'z');
+
+function high(x){
+  let splitString = x.split(" ");
+  let winnerWord = "";
+  let winnerScore = -1;
+  for (i = 0; i < splitString.length; i++) {
+    let word = splitString[i].split("")
+    let wordScore = 0;
+    for (j = 0; j < word.length; j++) {
+      let letterScore = alphabet.indexOf(word[j]) + 1;
+      wordScore += letterScore;
+    }
+    if (wordScore > winnerScore) {
+      winnerWord = splitString[i];
+      winnerScore = wordScore;
+    }
+  }
+return winnerWord;
+}
+//solution code
+
+Test.describe("Example tests",_=>{
+    Test.assertEquals(high('man i need a taxi up to ubud'), 'taxi');
+    Test.assertEquals(high('what time are we climbing up the volcano'), 'volcano'); 
+    Test.assertEquals(high('take me to semynak'), 'semynak');  
+    });
+    //sample tests
